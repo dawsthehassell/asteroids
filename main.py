e806b4ca607862c14a2359af49ad8824436c42ee
dawsthehassell <hasselldawson@gmail.com>
2024-12-12 09:09:15 -0600

    player move left/right

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
 
     while True:
+        dt = clock.tick(60) / 1000.0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0), rect=None, special_flags=0)
+        screen.fill((0,0,0))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000.0
 
 if __name__== "__main__":
     main()
